de7f36c97cf9960526ba39995992701a98ca2b37
Alex Guerrero <alex.guerrero178@gmail.com>
2026-03-18 08:22:03 -0400

    day 01 - introduction

---
+++ day_01/day_01.py
@@ -0,0 +1,34 @@
+# Day 1 - 30 Day of Python Challenge
+# Introduction
+
+# Checking operations
+print(3 + 4)             # addition(+)
+print(4 - 3)             # subtraction(-)
+print(3 * 4)             # multiplication(*)
+print(4 / 3)             # division(/)
+print(4 ** 3)            # exponential(**)
+print(4 % 3)             # modulus(%)
+print(4 // 3)            # floor division operator(//)
+
+# Checking strings
+print('Alex')            # name
+print('Guerrero')        # family name
+print('USA')             # country
+print('I am enjoying 30 days of python') # sentence
+
+# Checking data types
+print(type(10))          # Int
+print(type(9.8))         # float
+print(type(3.14))        # Float
+print(type(4 - 4j))      # Complex number
+print(type([1, 2, 3]))   # list
+print(type({'name':'Alex'})) # dictionary
+print(type({9.8, 3.14, 2.7}))    # set
+print(type((9.8, 3.14, 2.7)))    # tuple
+print(type('Alex'))      # string
+print(type('Guerrero'))
+print(type('USA'))
+print(type('I am enjoying 30 days of python'))
+
+# Finding an Euclidean distance between (2, 3) and (10, 8)
+print( (((2-10) ** 2) + ((3-8) ** 2)) ** (1/2))
