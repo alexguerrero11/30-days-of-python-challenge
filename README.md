e4ae6200e6184fb20b1521a59fea6b9d634f7796
Alex Guerrero <alex.guerrero178@gmail.com>
2026-03-23 09:02:51 -0400

    Day 03 - Operators - part one continue

--- README.md
+++ README.md
@@ -2,6 +2,10 @@
  My solutions and notes for the 30 Days of Python challenge
 
 ## Progress
-- [x] Day 01 - Introduction
-- [x] Day 02 - Variables, Built in Functions
-- [] Day 03 - Operations
+[x] Day 01 - Introduction
+[x] Day 02 - Variables, Built in Functions
+[] Day 03 - Operators
+[] Day 04 - Strings
+[] Day 05 - Lists
+[] Day 06 - Tuples
+[] Day 07 - Sets
--- day_03/day_03.py
+++ day_03/day_03.py
@@ -57,6 +57,7 @@ y_intercept = -2
 print('The slope is ', slope)
 print('The y-intercept is ', y_intercept)
 
+
 # Slope is (m = y2-y1/x2-x1). Find the slope and Euclidean distance between point (2, 2) and point (6,10)
 slope_2 = (10 - 2) / (6 - 2)
 distance = (((2-6) ** 2) + ((2-10) ** 2)) ** (1/2)
@@ -67,6 +68,9 @@ print('The distance is ', distance)
 
 # Compare the slopes in tasks 8 and 9.
 print('The first slope is', slope, '. The second slope is', slope_2)
+print('slope > slope_2: ', slope > slope_2)
+print('slope < slope_2: ', slope < slope_2 )
+print('slope == slope_2: ', slope == slope_2)
 
 
 # Calculate the value of y (y = x^2 + 6x + 9). Try to use different x values and figure out at what x value y is going to be 0.
@@ -79,6 +83,40 @@ print('The value of y (y = x^2 + 6x + 9) is ', y)
 # Find the length of 'python' and 'dragon' and make a falsy comparison statement.
 print('The length of python is ', len('python'))
 print('The length of dragon is ', len('dragon'))
+print(len('python') == len('dragon'))
 
 
 # Use and operator to check if 'on' is found in both 'python' and 'dragon'
+print('on in python', 'on' in 'python')
+print('on in dragon', 'on' in 'dragon')
+
+
+# I hope this course is not full of jargon. Use in operator to check if jargon is in the sentence.
+print('jargon in ')
+
+
+# There is no 'on' in both dragon and python
+
+
+# Find the length of the text python and convert the value to float and convert it to string
+
+
+# Even numbers are divisible by 2 and the remainder is zero. How do you check if a number is even or not using python?
+
+
+# Check if the floor division of 7 by 3 is equal to the int converted value of 2.7.
+
+
+# Check if type of '10' is equal to type of 10
+
+
+# Check if int('9.8') is equal to 10
+
+
+# Write a script that prompts the user to enter hours and rate per hour. Calculate pay of the person?
+
+
+# Write a script that prompts the user to enter number of years. Calculate the number of seconds a person can live. Assume a person can live hundred years
+
+
+# Write a Python script that displays the following table
